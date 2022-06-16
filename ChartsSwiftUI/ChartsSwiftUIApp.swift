a379675d2247afe9231e65b134993853056f23ee
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2022-06-16 17:30:38 +0900

    Initial Commit

---
+++ ChartsSwiftUI/ChartsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsSwiftUIApp.swift
+//  ChartsSwiftUI
+//
+//  Created by 伊藤明孝 on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct ChartsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
